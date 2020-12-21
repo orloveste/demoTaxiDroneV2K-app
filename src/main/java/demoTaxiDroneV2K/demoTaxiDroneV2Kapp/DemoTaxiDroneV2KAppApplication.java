0bb95c89b990de08e4d90643cf237c35eca414e7
orloveste <orlovevm@yahoo.com>
2020-12-21 10:31:43 +0200

    Initial commit

---
+++ src/main/java/demoTaxiDroneV2K/demoTaxiDroneV2Kapp/DemoTaxiDroneV2KAppApplication.java
@@ -0,0 +1,13 @@
+package demoTaxiDroneV2K.demoTaxiDroneV2Kapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTaxiDroneV2KAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTaxiDroneV2KAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demoTaxiDroneV2K/demoTaxiDroneV2Kapp/DemoTaxiDroneV2KAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demoTaxiDroneV2K.demoTaxiDroneV2Kapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTaxiDroneV2KAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
