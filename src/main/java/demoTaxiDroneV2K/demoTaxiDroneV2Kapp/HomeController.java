f38b0152d7b5b5f9753931001c955063759ab226
orloveste <orlovevm@yahoo.com>
2020-12-21 12:37:28 +0200

    Test and run app - display message on lh:8080//home

--- src/main/java/demoTaxiDroneV2K/demoTaxiDroneV2Kapp/HomeController.java
+++ src/main/java/demoTaxiDroneV2K/demoTaxiDroneV2Kapp/HomeController.java
@@ -1,4 +1,13 @@
 package demoTaxiDroneV2K.demoTaxiDroneV2Kapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/home")
+    public String test() {
+        return "index"; //view html page
+    }
 }
